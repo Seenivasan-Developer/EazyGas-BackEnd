a7af38c6b2e227bed8799e773c2739de50cf9bf8
Seenivasan-Developer <kbseenikb@gmail.com>
2024-06-02 22:55:14 +0530

    updated

--- models/BookingModel.js
+++ models/BookingModel.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema(
     {
-        providerdetails: { type: [{providername:{type:String, required:true}}, {providerid:{type:String, required:true}}], require: true },
+        providerdetails: {
+            type: {
+                providername: { type: String, required: true },
+                providerid: { type: String, required: true }
+            },
+            required: true
+        },
         BookingDate: { type: Date, require: true },
         address: { type: Array, require: true },
         selectedSlot: { type: String, require: true },
         selectedSlotType: { type: String, require: true },
         BPNo: { type: String, require: true },
         remarks: { type: String, require: true },
-        userDetails: { type: [{username:{type:String, required:true}}, {userid:{type:String, required:true}}], require: true },
+        userDetails: {
+            type: {
+                username: { type: String, required: true },
+                userid: { type: String, required: true }
+            },
+            required: true
+        },
         gasAmount: { type: Number, require: true },
         paymentMode: { type: String, require: true },
         isDelivered: { type: Boolean, require: true }
@@ -18,6 +30,6 @@ const BookingSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-const BookingModel=mongoose.model("Booking",BookingSchema);
+const BookingModel = mongoose.model("Booking", BookingSchema);
 
-module.exports=BookingModel;
+module.exports = BookingModel;
