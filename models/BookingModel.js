55b78dc5c231b32c83760466deba87ec32976eec
Seenivasan-Developer <kbseenikb@gmail.com>
2024-06-02 18:16:26 +0530

    booking updated

--- models/BookingModel.js
+++ models/BookingModel.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema(
     {
-        providerdetails: { type: Array, require: true },
+        providerdetails: { type: Array[{providername:{type:String, required:true}}, {providerid:{type:String, required:true}}], require: true },
         BookingDate: { type: Date, require: true },
         address: { type: Array, require: true },
         selectedSlot: { type: String, require: true },
         selectedSlotType: { type: String, require: true },
         BPNo: { type: String, require: true },
         remarks: { type: String, require: true },
-        userDetails: { type: Array, require: true },
+        userDetails: { type: Array[{username:{type:String, required:true}}, {userid:{type:String, required:true}}], require: true },
         gasAmount: { type: Number, require: true },
         paymentMode: { type: String, require: true },
         isDelivered: { type: Boolean, require: true }
